0b6e9f270977d95dd2b4556da45075aa196bf85e
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-08-22 01:35:27 +0000

    Used string interpolation to make output cleaner.

--- shape-main.jl
+++ shape-main.jl
@@ -4,8 +4,8 @@ function main()
     r=Shape.Rectangle(3,4)
     c=Shape.Circle(1)
 
-    println("Area of r: ",Shape.area(r))
-    println("Area of c: ",Shape.area(c))
+    println("Area of rectangle $r: $(Shape.area(r))")
+    println("Area of $c: $(Shape.area(c))")
 end
 
 main()
