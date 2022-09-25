49dc2a9570fa3b5385776fa7a2a1c92b39508a71
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-09-25 17:07:45 +0800

    Used JuliaFormat to reformat the code with format(filename).

--- atoi/atoi.jl
+++ atoi/atoi.jl
@@ -1,11 +1,10 @@
 using Logging
+
 function atoi(s::String)
-@debug "about atoi", s
-try
-    parse(Int32,s)
-catch e
-    @error e
+    @debug "about atoi", s
+    try
+        parse(Int32, s)
+    catch e
+        @error e
+    end
 end
-end
-
-
