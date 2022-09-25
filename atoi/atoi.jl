3a3937dbdc1718afb67f8830fd0259d956502be9
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-09-25 16:53:32 +0800

    Worked on explaining debug levels in atoi.

--- atoi/atoi.jl
+++ atoi/atoi.jl
@@ -1,5 +1,6 @@
 using Logging
 function atoi(s::String)
+@debug "about atoi", s
 try
     parse(Int32,s)
 catch e
--- atoi/main.jl
+++ atoi/main.jl
@@ -4,5 +4,8 @@ include("atoi.jl")
 demoAToI(s) = println("String $s to Int32 example: $(atoi(s))")
 
 demoAToI("23")
+
+# Enable debugging by setting ENV["JULIA_DEBUG"="main,atoi" -- note leave out the .jl extension.
+@debug "these should produce errors messages"
 demoAToI("123b")
 demoAToI("1.23")
